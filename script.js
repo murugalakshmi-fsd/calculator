84fbe8c937699d65f5f3c7df34e6568d77c745d6
Murugalakshmi muthukannan <150881194+murugalakshmi-fsd@users.noreply.github.com>
2023-12-18 15:25:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let outputScreen = document.getElementById("output-screen");
+function display(num){
+   
+    outputScreen.value += num;
+
+}
+function calculate()
+{
+    try{
+    outputScreen.value=eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid");
+    }
+
+}
+function cl(){
+outputScreen.value = "";
+}
+function del(){
+    outputScreen.value=outputScreen.value.slice(0,-1);
+}
